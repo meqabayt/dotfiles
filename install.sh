e218e754d151368ad487f93a756c92ca7fa2fd96
meqabayt <baytbybayt@gmail.com>
2023-02-07 16:19:33 +0400

    use relative path

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apk add git nodejs neovim ripgrep alpine-sdk --update
-mkdir -p /home/${USER}/.config
-git clone https://github.com/NvChad/NvChad /home/${USER}/.config/nvim
+mkdir ~/.config
+git clone https://github.com/NvChad/NvChad ~/.config/nvim
