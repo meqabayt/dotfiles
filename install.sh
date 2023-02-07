d53b1a7c1b91541a84c839d6701b0ea0014fa19c
meqabayt <baytbybayt@gmail.com>
2023-02-07 16:10:48 +0400

    change home

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 apk add git nodejs neovim ripgrep alpine-sdk --update
 mkdir -p /home/nvim/.config
-git clone https://github.com/NvChad/NvChad /home/nvim/.config/nvim
+git clone https://github.com/NvChad/NvChad /home/node/.config/nvim
