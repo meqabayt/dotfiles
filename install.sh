fcccd5629b24f7b9ce1a8bfe95438fe1741b1df0
meqabayt <baytbybayt@gmail.com>
2023-02-08 17:20:53 +0000

    add nerd font

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 sudo apt update
 sudo apt install git neovim -y
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
+unzip DroidSansMono.zip -d ~/.fonts
+fc-cache -fv
 mkdir -p ${HOME}/.config
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
