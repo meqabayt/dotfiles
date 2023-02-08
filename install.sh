c14674ecd28773f1f63f16ea97c73605727634c3
meqabayt <baytbybayt@gmail.com>
2023-02-08 16:53:01 +0000

    fix install sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install git nodejs neovim ripgrep alpine-sdk -y
+sudo apt install git neovim -y
 mkdir -p ${HOME}/.config
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
