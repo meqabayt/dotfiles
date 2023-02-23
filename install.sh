acf188ea5dbeebc69bb5d6c95e4211bc204cac42
meqabayt <baytbybayt@gmail.com>
2023-02-23 12:24:26 +0000

    install git and remove neovim

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+apt update
+apt remove neovim
+apt install -y git
+
 # install latest nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
